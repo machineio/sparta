a756b12523da0a40c3defce236faa0cf63f9205a
Jean Chassoul <chassoul@gmail.com>
2016-03-16 20:40:48 -0600

    sd

--- static/js/seed/fun.strings.js
+++ static/js/seed/fun.strings.js
@@ -1030,7 +1030,7 @@ fun.strings = {
     quoteUpdateRequest: "Quote Update Request",
     autoInsurancePolicyNumber: "Auto Insurance Policy Number",
     autoDocumentsNeeded: "Auto Documents Needed",
-    mailingZipCode: "Mailing Zip Code"
+    mailingZipCode: "Mailing Zip Code",
 
     systemMonitor: "System Monitor",
     goodBye: "Good bye",
