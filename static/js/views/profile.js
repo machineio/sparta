a4128abf115b33e823e84ff47be28bc54087b3f3
Alex Ramirez <alex.ramirez.valerin@gmail.com>
2016-04-06 17:24:13 -0600

    testing

--- static/js/views/profile.js
+++ static/js/views/profile.js
@@ -778,7 +778,7 @@ fun.views.profile = Backbone.View.extend({
                 $(profile_contact_modal_writing_agent.selector + " option[value='" + response.get('writing_agent') + "']").attr("selected", "selected");
 
                 profile_contact_modal_lead_type.val(response.get('lead_type') || 'health');
-                $(contact_lead_type.selector + " option[value='health']").attr("selected", "selected");
+                $(profile_contact_modal_lead_type.selector + " option[value='health']").attr("selected", "selected");
                 // $(profile_contact_modal_writing_agent.selector + " option[value='" + response.get('contact_lead_type') + "']").attr("selected", "selected");
 
                 profile_contact_modal_lead_source.val(response.get('lead_source') || 'boberdoo');
