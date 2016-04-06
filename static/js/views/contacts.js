75c76e1780da42703458eb1583f567ece0dadea8
Alex Ramirez <alex.ramirez.valerin@gmail.com>
2016-04-05 23:30:53 -0600

    testing

--- static/js/views/contacts.js
+++ static/js/views/contacts.js
@@ -412,6 +412,7 @@ fun.views.contacts = Backbone.View.extend({
         this.renderContactModalForm();
 
         function renderDate(date){
+            var now = new Date(date);
             var day = ("0" + now.getDate()).slice(-2);
             var month = ("0" + (now.getMonth() + 1)).slice(-2);
             var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
