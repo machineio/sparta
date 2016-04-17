fd06c2f9891649a01ab132733bddc265e11b41f3
Jean Chassoul <chassoul@gmail.com>
2016-04-17 15:12:30 -0700

    go!

--- static/js/seed/fun.application.js
+++ static/js/seed/fun.application.js
@@ -1,9 +1,34 @@
+/*
+* Fun namespace container
+*/
+var fun = {
+    account: {}, 
+    utils: {},
+    views: {},
+    instances: {},
+    containers: {},
+    models: {},
+    strings: {},
+    conf: {},
+    session: {}, //account and context maybe?
+    cache: {templates : {}},
+    omnibus: _.extend({}, Backbone.Events)
+};
+
 
 var app = Marionette.Application.extend({
   initialize: function(options) {
     console.log('My container:', options.container);
   }
 });
+
+
+
+// Although applications will not do anything
+// with a `container` option out-of-the-box, you
+// could build an Application Class that does use
+// such an option.
+var app = new app({container: '#app'});/*
 
 
 var my_template_html = '<div><%= args.name %></div>'
@@ -16,6 +41,6 @@ var MyView = Marionette.View.extend({
     });
   }
 });
-
+*/
 
 console.log('yeah alex');
