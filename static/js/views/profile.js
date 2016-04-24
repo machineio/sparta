d376212cdec7dd3d85a12df1fe9b3cc826d75b3c
Alex Ramirez <alex.ramirez.valerin@gmail.com>
2016-04-23 19:54:41 -0600

    testing

--- static/js/views/profile.js
+++ static/js/views/profile.js
@@ -1469,9 +1469,9 @@ fun.views.profile = Backbone.View.extend({
         list_of_stuff = new fun.models.ContactsContainer();
         list_of_stuff.fetch({data: $.param(data)});
 
-        console.log('DROPDOWN IN HERE!!!!',list_of_stuff.attributes.contacts.toJSON());
+        console.log('DROPDOWN IN HERE!!!!',list_of_stuff.attributes);
 
-        return list_of_stuff.attributes.contacts.toJSON();
+        return list_of_stuff.attributes;
     },
 
     editContact:function(event){
