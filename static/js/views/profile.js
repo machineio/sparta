384dccd4f043f0dacdcfda94a74adbe42447b25d
Alex Ramirez <alex.ramirez.valerin@gmail.com>
2016-04-25 07:31:23 -0600

    testing

--- static/js/views/profile.js
+++ static/js/views/profile.js
@@ -1104,7 +1104,7 @@ fun.views.profile = Backbone.View.extend({
 
         list_of_states = new fun.models.ContactsContainer();
         list_of_states.fetch({data: $.param(options), success: function(collection,response){
-            console.log('COLLECTION!!!',collection.toJSON());
+            console.log('COLLECTION!!!',collection.results.toJSON());
         }});
 
         return list_of_states.attributes.results;
