4e8dc24e53a01d408434dcce0d262c6eff534c41
Jean Chassoul <chassoul@gmail.com>
2016-05-01 14:48:16 -0700

    ok

--- static/js/views/contacts.js
+++ static/js/views/contacts.js
@@ -1744,6 +1744,8 @@ fun.layouts.contacts = Marionette.LayoutView.extend({
             fun.utils.getTemplate(fun.conf.templates.allContacts)
         );
 
+        console.log('mae que putas!');
+
         allContacts = this.$('#all-contacts-tab');
         allContacts.html(template);
 
