31ce7b66c1b98d74eaa080f1dcc94029fee2c58e
Alex Ramirez <alex.ramirez.valerin@gmail.com>
2016-05-04 20:02:22 -0600

    testing

--- static/js/views/profile.js
+++ static/js/views/profile.js
@@ -133,12 +133,7 @@ fun.views.profile = Backbone.View.extend({
             success: function(response){
 
                 contact_info_gender.value = response.get('contact_info_gender') || '';
-                $("" + contact_info_gender.selector + " option").filter(function() {
-                    //may want to use $.trim in here
-                    console.log('RESPONSE!!!',contact_info_gender.value,response.get('contact_info_gender'));
-                    return $(this).text() == response.get('contact_info_gender');
-                }).prop('selected', true);
-                // $(contact_info_gender.selector + " option[value='" + response.get('contact_info_gender') + "']").attr("selected", "selected");
+                $(contact_info_gender.selector + " option[value='" + response.get('contact_info_gender') + "']").attr("selected", "selected");
 
                 contact_info_marital_status.value = response.get('contact_info_marital_status') || '';
                 $(contact_info_marital_status.selector + " option[value='" + response.get('contact_info_marital_status') + "']").attr("selected", "selected");
